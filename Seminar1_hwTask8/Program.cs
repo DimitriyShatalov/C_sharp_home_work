d3109a74e53337e95301e4f1221d4e12a3512589
Dmitriy <dshatalov.expert@gmail.com>
2022-06-29 16:51:55 +0300

    add Seminar1 hwTask8 project

---
+++ Seminar1_hwTask8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите любое целое число ");
+int count = 0;
+int limit = Convert.ToInt32(Console.ReadLine());
+
+
+     while(count < limit - 1)
+    {
+        count = count + 2;
+        Console.WriteLine(count);
+    }
+    
