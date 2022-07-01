2f514178e7da30374a002c04413f45f72c4ff294
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-01 16:53:54 +0300

    Update Program.cs

--- Seminar2_hwTask10/Program.cs
+++ Seminar2_hwTask10/Program.cs
@@ -1,23 +1,23 @@
 ﻿/*
 Задача 10: Напишите программу, которая принимает на вход трёхзначное число
  и на выходе показывает вторую цифру этого числа.
-
 456 -> 5
 782 -> 8
 918 -> 1
 */
 
+//Решение, которое "не ругается" на преобразование литерала, допускающего значение NULL...
+int number = Convert.ToInt32(Console.ReadLine());
+String testNum = Convert.ToString(number);
+Console.WriteLine("" + testNum [2]);
 
-// Console.WriteLine("Введите трехзначное число: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-
-int number = new Random().Next(100,1000);
+//А можно было и так...
+// Console.WriteLine("Введите трехзначное число: "); // По желанию
+/*
+int number = Convert.ToInt32(Console.ReadLine()); // int number = new Random().Next(100,1000); - для разнообразия
 Console.WriteLine(number);
 
 int digitSecond = number / 10 % 10;
 
 Console.WriteLine(digitSecond);
-
-//int number = Convert.ToInt32(Console.ReadLine());
-//String testNum = Convert.ToString(number);
-//Console.WriteLine("" + testNum [2]);
+*/
