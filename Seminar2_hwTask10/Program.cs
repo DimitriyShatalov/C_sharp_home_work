a97e309417c0c0323d87f27cf27e5bf0c21d04c4
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-01 16:26:41 +0300

    Add files via upload

---
+++ Seminar2_hwTask10/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+
+int digitSecond = number / 10 % 10;
+
+Console.WriteLine(digitSecond);
+
+//int number = Convert.ToInt32(Console.ReadLine());
+//String testNum = Convert.ToString(number);
+//Console.WriteLine("" + testNum [2]);
