6a152be1a6e84340b00be2347fe66668f477d0d8
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-01 16:56:20 +0300

    Add files via upload

---
+++ Seminar2_hwTask13/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+// Console.WriteLine("Введите любое целое число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+
+// string testNum = Console.ReadLine();
+// Console.WriteLine("Третий символ " + testNum [2]);
+
+int number = Convert.ToInt32(Console.ReadLine());
+String testNum = Convert.ToString(number);
+
+int i = testNum.Length;
+
+    if(i <= 2)
+    {
+    Console.WriteLine("Третьей цифры нет ");
+    }
+    else
+        Console.WriteLine("" + testNum [2]);
