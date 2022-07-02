146a73839a83c078f5dde4a8aee6476c17b3fc7b
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-02 17:59:19 +0300

    Add files via upload

---
+++ Seminar3_hwTask19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int number = Convert.ToInt32(Console.ReadLine());
+string testNum = Convert.ToString(number);
+
+
+    if(testNum[0] == testNum[4] || testNum[1] == testNum[3])
+    Console.WriteLine("-> да ");
+    else
+    {
+        Console.WriteLine("-> нет ");
+    }
