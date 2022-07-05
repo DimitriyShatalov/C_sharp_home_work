412f6ca24a9860932b119262624be18605810116
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-05 16:04:11 +0300

    Add files via upload

---
+++ Seminar4_hwTask29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: 
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = GetNumberArray(8);
+Console.WriteLine($"[{String.Join("; ",array )}]");
+
+int[] GetNumberArray(int size)
+{
+    int[]resultArray = new int[size];
+    for(int i = 0; i<size; i++)
+    {
+        resultArray[i] = new Random().Next(9);
+    }
+    return resultArray;
+}
