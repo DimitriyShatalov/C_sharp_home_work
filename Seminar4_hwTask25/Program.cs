28cdc8e4800897b42456d084d04afe9968141f91
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-05 16:02:34 +0300

    Add files via upload

---
+++ Seminar4_hwTask25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Pow(A,B));
+
+// int A = new Random().Next(1,10);
+// int B = new Random().Next(1,10);
+
+// Console.WriteLine((A,B));
+// Console.WriteLine(Math.Pow(A,B));
