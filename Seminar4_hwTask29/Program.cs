e969a1782d4a59a75b36c67b80b77d4322c559e0
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-06 08:16:07 +0300

    Update Program.cs

--- Seminar4_hwTask29/Program.cs
+++ Seminar4_hwTask29/Program.cs
@@ -3,15 +3,32 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
 // 6, 1, 33 -> [6, 1, 33]
 
-int[] array = GetNumberArray(8);
+int[]array = new int[8];
+
+void FillArray(int[] collection)
+{
+    int length = array.Length;
+    int index = 0;
+     while(index < length)
+    {
+        array[index] = new Random().Next(1,9);
+
+        index++;
+    }
+}
+FillArray(array);
+
 Console.WriteLine($"[{String.Join("; ",array )}]");
 
-int[] GetNumberArray(int size)
-{
-    int[]resultArray = new int[size];
-    for(int i = 0; i<size; i++)
-    {
-        resultArray[i] = new Random().Next(9);
-    }
-    return resultArray;
-}
+// int[] array = GetNumberArray(8);
+// Console.WriteLine($"[{String.Join("; ",array )}]");
+
+// int[] GetNumberArray(int size)
+// {
+//     int[]resultArray = new int[size];
+//     for(int i = 0; i<size; i++)
+//     {
+//         resultArray[i] = new Random().Next(9);
+//     }
+//     return resultArray;
+// }
