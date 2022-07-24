c8bdfcd679cfe8f2112c4da3502dea773afb9656
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-07-24 15:57:33 +0300

    Update Program.cs

--- Seminar9_hw64_66_68/Program.cs
+++ Seminar9_hw64_66_68/Program.cs
@@ -4,10 +4,9 @@
 // Задача 64: 
 // Задайте значения M и N. 
 // Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
-
 // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 // M = 4; N = 8. -> ""4, 6, 7, 8""
-/*
+
 int M = Convert.ToInt32(Console.ReadLine());
 int N = Convert.ToInt32(Console.ReadLine());
 
@@ -19,20 +18,19 @@ string PrintNumbers(int start, int end)
     return (start + ", " + PrintNumbers(start + 1, end));
 }
 Console.ReadKey();
-*/
+
 
 // Задача 66: 
 // Задайте значения M и N. 
 // Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
-/*
+
 int numM = Convert.ToInt32(Console.ReadLine());
 int numN = Convert.ToInt32(Console.ReadLine());
 int m = numM;
-int n = numN;
-// Без переменных m и n код не работал. ???
+int n = numN;           // Без переменных m и n код не работал. ???
+
 int GaussSumNumbers(int n)
 {
     if (n == m) return m;
@@ -40,15 +38,15 @@ int GaussSumNumbers(int n)
 }
 Console.WriteLine($"Сумма чисел от {m} до {n} = {GaussSumNumbers(n)}");
 Console.ReadKey();
-*/
+
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 
 // m = 2, n = 3 -> A(m,n) = 29
 
-//Функция АккерманаЖ A(n,m) = {m+1 при n = 0; A(n-1),1 при n!=0, m = 0; A(n-1, A(n,m - 1)) при n>0, m>0
-/*
+//Функция Аккермана: A(n,m) = {m+1 при n = 0; A(n-1),1 при n!=0, m = 0; A(n-1, A(n,m - 1)) при n>0, m>0
+
 int argM = Convert.ToInt32(Console.ReadLine());
 int argN = Convert.ToInt32(Console.ReadLine());
 
@@ -57,11 +55,9 @@ int m = argM;
 int AckermannFunc(int n, int m)
 {
 if (n == 0) return m + 1;
-
 if (m == 0) return AckermannFunc(n - 1, 1);
 
 return AckermannFunc(n - 1, AckermannFunc(n, m - 1));
 }
 Console.WriteLine($"Функция Аккермана A({m},{n})= {AckermannFunc(m,n)}");
 Console.ReadKey();
-*/
