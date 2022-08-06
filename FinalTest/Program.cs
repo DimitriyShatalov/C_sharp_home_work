7886848b55a16d9e1b8f9fbdef7726487a7157d3
DimitriyShatalov <107166887+DimitriyShatalov@users.noreply.github.com>
2022-08-06 11:33:27 +0300

    Add folder with home work

---
+++ FinalTest/Program.cs
@@ -0,0 +1,32 @@
+﻿
+string[] arrString = { "123", "1234", "12345", "one", "seven", "ok", ":)" };
+int newArrayLength = 0;
+
+Console.WriteLine("Изначально заданный массив: ");
+Console.WriteLine($"{String.Join("; ",arrString )}");
+
+for (int i = 0; i < arrString.Length; i++)
+{
+    if (arrString[i].Length <= 3)
+    {
+        newArrayLength++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Финальный массив с элементами, у которых три и меньше символов: ");
+
+string[] newArray = new string[newArrayLength];
+int k = 0;
+for (int i = 0; i < arrString.Length; i++)
+{
+    if (arrString[i].Length <= 3)
+    {
+        newArray[k] = arrString[i];
+        //Console.Write($"{newArray[k]}" + " ");
+        k++;
+    }
+}
+Console.WriteLine($"{String.Join("; ",newArray )}");
+Console.ReadKey();
+
